c02b22990ea7ebe16aed8221ad975b8270498591
R S <rspet123@gmail.com>
2021-11-10 16:28:34 -0500

    Add files via upload

--- Spiderfy.py
+++ Spiderfy.py
@@ -1,17 +1,13 @@
-# Spotify API Test
 import networkx as nx
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 import spotipy
-import os
 from spotipy.oauth2 import SpotifyClientCredentials
+from tqdm import tqdm
 # Porter Robinson: 3dz0NnIZhtKKeXZxLOxCam
 # Seven Lions: 6fcTRFpz0yH79qSKfof7lp
 # Last Heros: 3HHfEn7yPOy3IiHS6CHG97
 # Caster: 4z7OnrBHTHdfpjNKl7NGox
-SPOTIPY_CLIENT_ID = 'xx'
-SPOTIPY_CLIENT_SECRET = 'xx'
+SPOTIPY_CLIENT_ID = '9a243ad77a18419198c066da092d1004'
+SPOTIPY_CLIENT_SECRET = '0b1370eb5dfa46129266b7aebc5b66e7'
 max_depth = 0
 depth = 0
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET))
@@ -19,6 +15,9 @@ start_uri = 'Not A Uri'
 search_name = "Not A Name"
 artist_list = []
 cont = "Y"
+artist_pop = []
+filter_num = -1
+
 
 while not cont.upper() == "N":
     search_name = input("Search Artist: ")
@@ -43,20 +42,29 @@ while max_depth == 0:
         max_depth = int(input("Enter Depth of Web: "))
     except ValueError:
         print("NaN Try again")
-
+        
+while filter_num == -1:
+    try:
+        filter_num = int(input("Enter Filter Number: "))
+    except:
+        print("Setting to 1 (Default)")
+        filter_num = 1
+descript = match_list[i]["name"]
 visited = []
 #artist_list.append(start_uri)
 graph = nx.Graph()
 edge_labler = {}
 
-while depth < max_depth:
-    depth = depth+1
+for d in range(max_depth):
+    #depth = depth+1
     try:
         current_artist = artist_list.pop(0)
     except IndexError:
         continue
+    
     album_types = ['album','single']
     artist_name = spotify.artist(current_artist)["name"]
+    descript = (artist_name + " " + str(d) + '/' + str(max_depth))
     for a_type in album_types:
         results = spotify.artist_albums(current_artist, album_type=a_type)
         albums = results['items']
@@ -64,9 +72,9 @@ while depth < max_depth:
         while results['next']:
             results = spotify.next(results)
             albums.extend(results['items'])
-            relateds = spotify.artist_related_artists(current_artist)
+            #relateds = spotify.artist_related_artists(current_artist)
             #print(relateds)
-        for album in albums:
+        for album in tqdm(albums,desc =  (descript + "|" + a_type + 's')):
             #print("Album Name:" + album["name"] +" ID"+ album["id"])
             tracks = spotify.album_tracks(album["id"])
             for track in tracks["items"]:
@@ -88,8 +96,14 @@ while depth < max_depth:
                         elif not artist["uri"] == current_artist:
                             graph.add_edge(artist_name,artist["name"])
                             #print("e") #Add Edge to graph
+remove_node = []                        
+for node in graph.degree():
+    if node[1] < filter_num:
+        remove_node.append(node[0])
+graph.remove_nodes_from(remove_node)
 pos = nx.spring_layout(graph)
 #pos = nx.kamada_kawai_layout(graph)
+
 nx.draw(graph,pos, with_labels=True,edge_color='pink')
 #nx.draw_networkx_edge_labels(graph,pos, edge_labels = edge_labler)
 print("Done")
